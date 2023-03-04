6fdb60fcde98587866212671e66fde1175c25814
mwoodpatrick <mwoodpatrick@gmail.com>
2023-03-04 10:47:31 -0800

    updated k3s script

--- kube/k3s/init.bash
+++ kube/k3s/init.bash
@@ -1,35 +1,36 @@
+# https://docs.k3s.io/quick-start
+
 echo "BASH_SOURCE=${BASH_SOURCE[0]}"
-export K3S_HOME=`dirname $(realpath ${BASH_SOURCE[0]})`
+export K3S_HOME=$(dirname $(realpath ${BASH_SOURCE[0]}))
 echo "K3S_HOME=${K3S_HOME}"
 
 function k3s_configure {
-    export K3S_DIR=${HOME}/.k3s
+	export K3S_DIR=${HOME}/.k3s
 
-    unset KUBECONFIG
-    
-    alias kubectl='f(){ sudo k3s kubectl "$@";  unset -f f; }; f'
+	unset KUBECONFIG
+
+	alias kubectl='f(){ sudo k3s kubectl "$@";  unset -f f; }; f'
 
 	mkdir -p ${K3S_DIR}
 
 	# check if k3s installed
-	if ! command -v k3s
-	then
+	if ! command -v k3s; then
 		# install k3s
 		curl -sfL https://get.k3s.io | sh -
 	fi
 
 	# create bash completions
 
-    if [ -f "${K3S_DIR}/bash_completion" ]; then
-	    k3s completion bash > ${K3S_DIR}/bash_completion
-    fi
+	if [ -f "${K3S_DIR}/bash_completion" ]; then
+		k3s completion bash >${K3S_DIR}/bash_completion
+	fi
 
-    . "${K3S_DIR}/bash_completion" # This loads k3s bash_completion
+	. "${K3S_DIR}/bash_completion" # This loads k3s bash_completion
 }
 
 function k3s_dependency_issues {
-    # check for any dependency issues
-    k3s sysinfo|grep -v pass
+	# check for any dependency issues
+	k3s sysinfo | grep -v pass
 }
 
 # Check service, logs and k3s status
@@ -39,9 +40,14 @@ function k3s_status {
 }
 
 function k3s_update_configuration {
-    echo "not implemented"
+	echo "not implemented"
 }
 
 function k3s_remove {
-    echo "not implemented"
+	echo "not implemented"
+}
+
+function k3s_list_containers {
+	# add -a to include stopped containers
+	sudo k3s crictl ps
 }
