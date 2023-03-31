a558a3cc1125c2cb9a03307f5161d30a2bea98be
mwoodpatrick <mwoodpatrick@gmail.com>
2023-03-31 02:51:58 -0700

    add Kubernetes Dashboard

--- kube/westie-dev/start.sh
+++ kube/westie-dev/start.sh
@@ -13,6 +13,7 @@ source ./config.sh
 # remove cluster if it exists
 if [[ ! -z $(k3d cluster list | grep "^${CLUSTER}") ]]; then
   echo
+  echo "KUBERNETES_DASHBOARD_ENABLE: ${KUBERNETES_DASHBOARD_ENABLE} - controls if Kubernetes Dashboard is deployed."
   echo "==== $0: remove existing cluster"
   read -p "K3D cluster \"${CLUSTER}\" exists. Ok to delete it and restart? (y/n) " -n 1 -r
   echo
@@ -31,6 +32,34 @@ rm /tmp/k3d-config.yaml
 export KUBECONFIG=$(k3d kubeconfig write ${CLUSTER})
 echo "export KUBECONFIG=${KUBECONFIG}"
 
+echo
+echo "==== $0: Loading helm repositories"
+if [ "${KUBERNETES_DASHBOARD_ENABLE}" == "yes" ]; then
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard  # kubernetes-dashboard
+fi
+
+helm repo update
+
+#
+# deploy kubernetes dashboard
+if [ "${KUBERNETES_DASHBOARD_ENABLE}" == "yes" ]; then
+
+  #
+  # deploy Kubernetes Dashboard
+  (cd kubernetes-dashboard; ./kubernetes-dashboard-deploy.sh)
+
+  #
+  # get the token for display at the end of this script
+  SECRET=$(kubectl get secret -n kubernetes-dashboard | grep kubernetes-dashboard-token- | cut -d " " -f 1)
+  TOKEN=$(kubectl -n kubernetes-dashboard describe secret ${SECRET}  | grep ^token | cut -d " " -f 7)
+
+fi
+
 echo 
 echo "==== $0: Various information"
 echo "export KUBECONFIG=${KUBECONFIG}"
+if [ "${KUBERNETES_DASHBOARD_ENABLE}" == "yes" ]; then
+  echo "kubernetes dashboard:"
+  echo "   visit http://localhost:${HTTPPORT}/dashboard/#/workloads?namespace=_all"
+  echo "   use token: ${TOKEN}"
+fi
