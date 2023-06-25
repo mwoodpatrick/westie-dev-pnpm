c0ad0958fe9178db1059ee313fd1be972d566104
mwoodpatrick <mwoodpatrick@gmail.com>
2023-06-25 04:32:15 -0700

    Update arch deployment

--- kube/westie-dev/ingress/deploy.bash
+++ kube/westie-dev/ingress/deploy.bash
@@ -1,3 +1,6 @@
 kubectl apply -f apple.yaml
 kubectl apply -f banana.yaml
+kubectl apply -f nginx_deployment.yaml
 kubectl create -f ingress.yaml
+kubectl create -f hello-ingress.yaml
+kubectl create -f simple-web-server-with-ingress.yaml
