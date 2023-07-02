038a2ecc7cc31236406ad19d0eb7896c29a12f66
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-01 22:12:00 -0700

    update kong test script

--- kube/westie-dev/ingress/kong1/test.bash
+++ kube/westie-dev/ingress/kong1/test.bash
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
 # https://minikube.sigs.k8s.io/docs/handbook/addons/kong-ingress/
 
-k apply -f echo_service.yaml
-k apply -f kong_ingress.yaml
-k get ingress
+# which bash
+# alias
+NS=test-kong
+k create namespace $NS
+k --namespace $NS apply -f echo_service.yaml
+k --namespace $NS apply -f kong_ingress.yaml
+k --namespace $NS get ingress
+sleep 5
 curl -i localhost/echo -H "Host: kong.example"
+k --namespace $NS delete -f kong_ingress.yaml
+k --namespace $NS delete -f echo_service.yaml
+k delete namespace $NS
