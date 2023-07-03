c38f249b0176db648fea5c824c25d0bdc7aa7304
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-03 03:07:55 -0700

    Fix issues with test script

--- kube/westie-dev/ingress/kong1/test.bash
+++ kube/westie-dev/ingress/kong1/test.bash
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
+# Test Kong Ingress
 # https://minikube.sigs.k8s.io/docs/handbook/addons/kong-ingress/
 
-# which bash
-# alias
 NS=test-kong
-k create namespace $NS
-k --namespace $NS apply -f echo_service.yaml
-k --namespace $NS apply -f kong_ingress.yaml
-k --namespace $NS get ingress
+kubectl create namespace $NS
+kubectl --namespace $NS apply -f kong_ingress.yaml
+kubectl --namespace $NS get ingress
 sleep 5
 curl -i localhost/echo -H "Host: kong.example"
-k --namespace $NS delete -f kong_ingress.yaml
-k --namespace $NS delete -f echo_service.yaml
-k delete namespace $NS
+kubectl --namespace $NS delete -f kong_ingress.yaml
+kubectl --namespace $NS delete -f echo_service.yaml
+kubectl delete namespace $NS
