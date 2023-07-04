3e882eb1bb82d5757f64491f9ddae49670a7572f
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-04 11:50:50 -0700

    cleanup hostpath example

---
+++ kube/westie-dev/storage/hostpath/test.bash
@@ -0,0 +1,9 @@
+# https://www.kubermatic.com/blog/keeping-the-state-of-apps-1-introduction-to-volume-and-volumemounts/
+set -x
+kubectl create -f hostpath-volume.yaml
+kubectl wait -f hostpath-volume.yaml  --for=condition=Ready
+kubectl exec  my-hostpath-app -- ls -l /projects/data/foo
+kubectl exec  my-hostpath-app -- cat /projects/data/foo
+cat /mnt/wsl/projects/data/foo
+kubectl delete -f hostpath-volume.yaml
+kubectl get pods
