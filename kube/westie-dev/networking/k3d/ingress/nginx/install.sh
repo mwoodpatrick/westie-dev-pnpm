f21b09835b016a0fb99ec94d303f271a6e0e4d7e
mwoodpatrick <mwoodpatrick@gmail.com>
2023-07-16 04:30:34 -0700

    k3d nginx ingress example

---
+++ kube/westie-dev/networking/k3d/ingress/nginx/install.sh
@@ -0,0 +1,46 @@
+# https://k3d.io/v5.0.1/usage/exposing_services Ingress
+# http://vault13.co.uk/local-kubernetes-with-k3d-and-nginx-ingress/
+set -x
+# configure k3s‘s API-Server listening on port 6550 with that port mapped to the host system.
+# all ports exposed on the serverlb will be proxied to the same ports on all server nodes in the cluster
+# Map port 80 on loadbalancer to localhost:8081
+# Docker shows:
+# c1247ddbba37   ghcr.io/k3d-io/k3d-proxy:5.5.1   "/bin/sh -c nginx-pr…"   2 minutes ago   Up 2 minutes   0.0.0.0:8081->80/tcp, :::8081->80/tcp, 0.0.0.0:6550->6443/tcp     k3d-k3s-default-serverlb
+k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 2 --k3s-arg "--disable=traefik@server:0" --wait
+
+# install nginx ingress
+
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+
+# Create a nginx deployment 
+kubectl apply -f nginx.yaml
+
+until kubectl apply -f nginx.yaml
+do
+  sleep 10
+done
+
+# wait on ingress to be ready
+# kubectl wait --for=condition=ready svc -l app=nginx
+
+ip=""
+while [ -z $ip ]; do
+  echo "Waiting for external IP"
+  ip=$(kubectl get ingress/nginx-ingress --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  [ -z "$ip" ] && sleep 10
+done
+echo 'Found external IP: '$ip
+
+docker ps
+kubectl get nodes
+kubectl get svc
+kubectl get ingress/nginx
+
+# Use curl to access the nginx web app
+
+until curl --fail localhost:8081/
+do
+  sleep 0.1
+done
+
+set +x
