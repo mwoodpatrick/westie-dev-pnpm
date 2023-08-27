24e5bce78c4b54370aac3da2190c52eadaeddef8
mwoodpatrick <mwoodpatrick@gmail.com>
2023-08-27 04:53:34 -0700

    update starter scripts

--- .devcontainer/scripts/run.sh
+++ .devcontainer/scripts/run.sh
@@ -4,4 +4,4 @@
 #  --env-file .env
 #
 
-docker run --name westie-dev -p 127.0.0.1:8080:80/tcp -w ${HOME} -t -i  -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:${HOME} -v /mnt/g:/google-drive -v /mnt/wsl/projects:/projects westie-dev bash
+docker run --name westie-dev --network host -w ${HOME} -t -i  -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:${HOME} -v /mnt/g:/google-drive -v /mnt/wsl/projects:/projects westie-dev bash
