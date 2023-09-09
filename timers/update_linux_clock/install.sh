217b71568c15c90192b085d060de63a3951b9cd1
mwoodpatrick <mwoodpatrick@gmail.com>
2023-09-09 14:20:56 -0700

    update clock synching

--- timers/update_linux_clock/install.sh
+++ timers/update_linux_clock/install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-# https://wiki.archlinux.org/title/Systemd/Timers
-# https://www.digitalocean.com/community/tutorials/understanding-systemd-units-and-unit-files
-# https://www.baeldung.com/linux/kernel-buffer-add-messages
 
 set -x
 dir=$(realpath $( dirname "${BASH_SOURCE[0]}" ))
 echo $dir
-# sudo systemctl stop update_linux_clock.timer
-sudo cp $dir/update_linux_clock* /etc/systemd/system
-sudo systemctl restart update_linux_clock.timer
+sudo cp $dir/update_linux_clock.timer /etc/systemd/system
 sudo systemctl daemon-reload
+sudo systemctl start update_linux_clock.timer
